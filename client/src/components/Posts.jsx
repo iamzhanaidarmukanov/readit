2009a7726190a4819a9358957a176ac6f67be652
iamzhanaidarmukanov <iamzhanaidar.mukanov@gmail.com>
2022-06-12 18:23:55 +0600

    Posts Styling Complete

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -3,20 +3,11 @@ import Post from './Post'
 
 const Posts = () => {
   return (
-    <div className="m-4 flex flex-1
-      2xl:min-w-[450px]
-      2xl:max-w-[500px]
-      sm:min-w-[270px]
-      sm:max-w-[300px]
-      min-w-full
-      flex-row p-3 rounded-md hover:shadow-2xl"
-    >
-    <div className='flex flex-col items-center w-full mt-3'>
-      <div className="display-flex justify-start w-full mb-6 p-2">
-        <Post />
-        <Post />
-      </div>
-    </div>
+    <div className='grid grid-cols-3 gap-2 m-[0.5em] ml-[1em]'>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
     </div>
   )
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     extend: {
       fontFamily: {
         fancy: ["Brush Script MT", "cursive"],
+        bookheading: ["Bree Serif", "serif"],
+        review: ["EB Garamond", "serif"],
+        edit: ["Farsan", "cursive"],
       },
     },
   },
