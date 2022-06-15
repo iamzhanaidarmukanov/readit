d82527a608b1ba15eb150e90f44d988b47604d0b
iamzhanaidarmukanov <iamzhanaidar.mukanov@gmail.com>
2022-06-15 08:14:12 +0600

    Backend Side: Post Update Routes are done

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -20,3 +20,11 @@ export const createPost = async (req, res) => {
     res.status(409).json({ message: error.message });
   }
 };
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("No Post with that id");
+  PostMessage.findByIdAndUpdate(_id, post, { new: true });
+};
