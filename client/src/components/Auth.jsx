fb2f0ca5301108fc6e79868449e9ee2e5321e088
iamzhanaidarmukanov <iamzhanaidar.mukanov@gmail.com>
2022-06-16 18:32:09 +0600

    Google Authentication : Layout

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import {GoogleLogin} from 'react-google-login'
 
 const Auth = () => {
   const [isSignUp, setIsSignUp] = useState(false)
@@ -12,6 +13,15 @@ const Auth = () => {
   const changeMode = () => {
     setIsSignUp(!isSignUp)
   }
+
+
+  const googleSuccess = () => {
+
+  }
+  const googleFailure = () => {
+    
+  }
+
   return (
     <div className='shadow-[0_5px_10px_-1px_grey] mx-auto bg-white rounded w-full md:w-1/2  p-5 m-y-5 m-[0.5em]'>
       <img src="https://static.vecteezy.com/system/resources/previews/001/200/107/original/book-png.png" alt="book" className='w-12 mx-auto' />
@@ -36,12 +46,32 @@ const Auth = () => {
         }
         <br />
         <div className='w-[100px] mx-auto my-2 cursor-pointer bg-[#809fff] hover:bg-[#1a66ff] text-white font-review rounded'>
-          <button>Sign In</button>
+          <button>{isSignUp ? "Sign Up" : "Sign In"}</button>
         </div>
         <div className='text-gray-500 hover:text-gray-800'>
         <button onClick={changeMode}>{isSignUp ? "Already have an account? Sign In" : "Don't have an account? Sign Up"}</button>
         </div>
       </form>
+
+      <div class="font-review flex justify-between items-center mt-3">
+        <hr class="w-full"/>
+          <span class="p-2 text-gray-400 mb-1">OR</span>
+        <hr class="w-full"/>
+      </div>
+
+        <GoogleLogin 
+          clientId='GOOGLE ID'
+          render = {(renderProps) => (
+            <button className="h-8 text-white w-full rounded-lg bg-red-700 hover:bg-red-900" onClick={renderProps.onClick} disabled={renderProps.disabled}>
+              <i className="fa fa-google mr-2" />
+              Google
+            </button>
+          )}
+          onSuccess={googleSuccess}
+          onFailure={googleFailure}
+          cookiePolicy="single_host_policy"
+          />
+
     </div>
   )
 }
