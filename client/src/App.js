6ae4cea368ce0004365de2d886ef85a101857c56
iamzhanaidarmukanov <iamzhanaidar.mukanov@gmail.com>
2022-06-16 16:36:10 +0600

    Switch changed to Routes apparently

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Header, Home, Auth } from "./components";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className="min-h-screen gradient-bg-main">
         <Header />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/auth" exact component={Auth} />
-        </Switch>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/auth" element={<Auth />} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
